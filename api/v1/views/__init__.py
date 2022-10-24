9d1ad561856c8ed3f4f6b8cab20b158b45259eba
bryansomto <bryansomto@gmail.com>
2022-10-24 11:50:02 +0100

    extra modif

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Exports app_views"""
 from flask import Blueprint
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')  # noqa
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.states import *
 from api.v1.views.cities import *
