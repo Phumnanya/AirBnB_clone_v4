cb727972b7bb3bbe558b8ac583e983995fa7afd3
Bryan Somto <bryansomto@gmail.com>
2022-10-24 18:29:00 +0100

    extra modif

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,7 +2,7 @@
 """Initialize Blueprint views"""
 from flask import Blueprint
 
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")  #noqa
 
 from api.v1.views.index import *
 from api.v1.views.amenities import *
