28691afd9fdecb0dda3ef06ec771f68080749d52
Bryan Somto <bryansomto@gmail.com>
2022-10-31 18:41:55 +0100

    added advanced task - 6

--- web_dynamic/static/scripts/100-hbnb.js
+++ web_dynamic/static/scripts/100-hbnb.js
@@ -1,16 +1,34 @@
 const $ = window.$;
 $(document).ready(function () {
   const amenities = {};
-  $('input[type="checkbox"]').change(function () {
+  $('.amenities INPUT[type="checkbox"]').change(function () {
     if ($(this).is(':checked')) {
       amenities[$(this).attr('data-id')] = $(this).attr('data-name');
     } else {
       delete amenities[$(this).attr('data-id')];
     }
-    $('#amenities_list').text(Object.values(amenities).join(', '));
+    $('.amenities H4').text(Object.values(amenities).join(', '));
+  });
+  const cities = {};
+  $('.locations INPUT[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      cities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete cities[$(this).attr('data-id')];
+    }
+    $('.locations H4').text(Object.values(cities).join(', '));
+  });
+  const states = {};
+  $('.locations INPUT[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      states[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete states[$(this).attr('data-id')];
+    }
+    $('.locations H4').text(Object.values(states).join(', '));
   });
   $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
-	  if (data.status === 'OK') {
+    if (data.status === 'OK') {
       $('#api_status').addClass('available');
     } else {
       $('#api_status').removeClass('available');
@@ -47,7 +65,7 @@ $(document).ready(function () {
       url: 'http://0.0.0.0:5001/api/v1/places_search/',
       dataType: 'json',
       contentType: 'application/json',
-      data: JSON.stringify({ amenities: Object.keys(amenities) }),
+      data: JSON.stringify({ amenities: Object.keys(amenities), cities: Object.keys(cities), states: Object.keys(states) }),
       success: function (response) {
         for (let i = 0; i < response.length; i++) {
           $('.places').append(
